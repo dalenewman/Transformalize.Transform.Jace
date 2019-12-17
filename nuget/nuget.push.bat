502fd3e82837afd1bddf80f808e506b92e515096
Dale Newman <dalenewman@gmail.com>
2019-12-17 10:21:44 -0500

    update dependencies

--- nuget/nuget.push.bat
+++ nuget/nuget.push.bat
@@ -1,2 +1,2 @@
-REM nuget push "c:\temp\modules\Transformalize.Transform.Jace.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Jace.Autofac.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Jace.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Jace.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
